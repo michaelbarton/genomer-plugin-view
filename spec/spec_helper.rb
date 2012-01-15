8514b84d1d621839f033ab445fe1150748c98a8d
Michael Barton <mail@michaelbarton.me.uk>
2012-01-15 18:18:33 -0500

    Add gff3 record helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'scaffolder/test/helpers'
 require 'genomer-plugin-view'
+require 'genomer-plugin-view/gff_record_helper'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -12,4 +13,5 @@ RSpec.configure do |config|
   config.mock_with :rr
 
   include Scaffolder::Test
+  Scaffolder::Test::Annotation.send(:include, GenomerPluginView::GffRecordHelper)
 end
