4e3bedf9ab404b90d0a232cf6a077cae9aee1a6a
Michael Barton <mail@michaelbarton.me.uk>
2012-01-30 16:55:33 -0500

    Specs for numbering locus tags from origin

--- spec/genomer-view-plugin/table_spec.rb
+++ spec/genomer-view-plugin/table_spec.rb
@@ -27,24 +27,44 @@ describe GenomerPluginView::Table do
 
   describe "with one annotation" do
 
-    let(:flags){ {:identifier => 'name'} }
-
     let(:annotations) do
       [Annotation.new(
         :seqname    => 'seq1',
         :start      => 1,
         :end        => 3,
         :feature    => 'gene',
-        :attributes => {})]
+        :attributes =>  {'ID' => 'gene1'})]
     end
 
-    it "should return the header line and annotation" do
-      subject.run.should == <<-EOS.unindent
-      >Feature\tname\tannotation_table
-      1\t3\tgene
-      EOS
+    describe "and only the identifier flag" do
+
+      let(:flags){ {:identifier => 'name'} }
+
+      it "should return the header line and annotation" do
+        subject.run.should == <<-EOS.unindent
+        >Feature\tname\tannotation_table
+        1\t3\tgene
+        \t\t\tlocus_tag\tgene1
+        EOS
+      end
+
+    end
+
+    describe "and the number from origin flag" do
+
+      let(:flags){ {:identifier => 'name', :number_from_origin => true} }
+
+      it "should return the header line and annotation" do
+        subject.run.should == <<-EOS.unindent
+        >Feature\tname\tannotation_table
+        1\t3\tgene
+        \t\t\tlocus_tag\t000001
+        EOS
+      end
+
     end
 
   end
 
 end
+
