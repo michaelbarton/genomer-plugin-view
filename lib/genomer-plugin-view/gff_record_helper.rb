404118e160124354b97496ac8892b8dac6f3572f
Michael Barton <mail@michaelbarton.me.uk>
2012-02-16 14:17:31 -0500

    Remove unused gff3 helper methods

--- lib/genomer-plugin-view/gff_record_helper.rb
+++ lib/genomer-plugin-view/gff_record_helper.rb
@@ -28,23 +28,6 @@ module GenomerPluginView::GffRecordHelper
     end
   end
 
-  def to_genbank_feature_row
-    out = [coordinates]
-    attributes.each do |atr|
-      out << process(atr)
-    end
-    out
-  end
-
-  def process(attr)
-    key, value = attr
-    case key
-    when 'ID'   then ['locus_tag', value]
-    when 'Name' then ['gene', value]
-    else return attr
-    end
-  end
-
   def to_genbank_table_entry
     delimiter = "\t"
     indent    = delimiter * 2
